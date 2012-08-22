6a685234ee10aa0976500062976ffe0d7e751836
stefan <stefan@laptop.(none)>
2012-08-22 14:19:34 -0400

    started script for initial vim setups

---
+++ vim/init.setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/jasom/vimball-pathogen.git
+git clone git://vim-latex.git.sourceforge.net/gitroot/vim-latex/vim-latex  
+git clone https://github.com/vim-scripts/YankRing.vim.git
+git clone https://github.com/mileszs/ack.vim.git
+git clone git://git.wincent.com/command-t.git
