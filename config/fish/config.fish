d146f00eebe02d56de79493fd1a1b6a2af590e5c
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-09-14 06:01:52 -0400

    fish alias setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,9 @@ set -g OMF_PATH $HOME/.local/share/omf
 # Path to your oh-my-fish configuration.
 set -g OMF_CONFIG $HOME/.config/omf
 
+# Path to your oh-my-fish.
+set -g ALIAS_PATH $HOME/.config/fish
+
 ### Configuration required to load oh-my-fish ###
 # Note: Only add configurations that are required to be set before oh-my-fish is loaded.
 # For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
@@ -12,4 +15,5 @@ set -g OMF_CONFIG $HOME/.config/omf
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-alias gs "git status"
+# Load alias definitions
+source $ALIAS_PATH/alias.fish
