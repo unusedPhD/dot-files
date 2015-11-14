393bff46de4d9d32009e37ad6331c51fe7ffd016
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-11-14 17:38:51 -0500

    basic bootstrap script to setup new env

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+rm ~/.bashrc
+ln -s ~/code/dot-files/bashrc ~/.bashrc
+rm ~/.vimrc
+ln -s ~/code/dot-files/vimrc ~/.vimrc
+ln -s ~/code/dot-files/vimrc.bundles ~/.vimrc.bundles
+ln -s ~/code/dot-files/gitconfig ~/.gitconfig
+ln -s ~/code/dot-files/gitignore ~/.gitignore
+ln -s ~/code/dot-files/tmux.conf ~/.tmux.conf
+rm ~/.config/user-dirs.dirs
+rm ~/.config/user-dirs.locale
+ln -s ~/code/dot-files/config/user-dirs.dirs ~/.config/user-dirs.dirs
+ln -s ~/code/dot-files/config/user-dirs.locale ~/.config/user-dirs.locale
+
+
+
+
