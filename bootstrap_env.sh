104c1f9b7d2e9b4226e2dcf4cee4210da188f866
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-12-13 11:45:52 -0500

    vim folder created and powerline submodule removed

--- bootstrap_env.sh
+++ bootstrap_env.sh
@@ -3,8 +3,8 @@ sudo apt-get install git tmux vim-nox
 rm ~/.bashrc
 ln -s ~/code/dot-files/bashrc ~/.bashrc
 rm ~/.vimrc
-ln -s ~/code/dot-files/vimrc ~/.vimrc
-ln -s ~/code/dot-files/vimrc.bundles ~/.vimrc.bundles
+ln -s ~/code/dot-files/vim/vimrc ~/.vimrc
+ln -s ~/code/dot-files/vim/vimrc.bundles ~/.vimrc.bundles
 ln -s ~/code/dot-files/gitconfig ~/.gitconfig
 ln -s ~/code/dot-files/gitignore ~/.gitignore
 ln -s ~/code/dot-files/tmux.conf ~/.tmux.conf
