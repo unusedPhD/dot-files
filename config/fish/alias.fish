bc1d42d0849942ff449c4b09d6344b710fd50536
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-12-26 07:40:52 -0500

    bashrc cleanup and fish aliases

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -1,2 +1,29 @@
+# dir navigation
+alias ..   "cd ../"
+alias 2.   "cd ../"
+alias ...  "cd ../../"
+alias 3.   "cd ../../"
+alias .... "cd ../../../"
+alias 4.   "cd ../../../"
+
+# ls
+alias l  "ls -1"
+alias ll "ls -haltr"
+
+# git
 alias gs "git status"
 alias gh "git hist"
+alias gb "git branch"
+alias gd "git diff"
+
+# typos
+alias xs "cd"
+alias vf "cd"
+alias got "git"
+alias get "git"
+alias vmi "vim"
+alias claer "clear"
+alias clera "clear"
+alias celar "clear"
+alias mann "man"
+
